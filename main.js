bc5f8f75b7c3109884da61e615bee828209e69eb
Md. Saiful Alam <saifulp4200@gmail.com>
2021-04-09 15:44:55 +0600

    final submit

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ searchBtn.addEventListener('click', () => {
     const inputCountry = document.getElementById('Country').value;
     getCountryName(inputCountry)
 })
+
 const Cinfo = data => {
     const info = data[0];
     const imgg = info.flag;
@@ -19,13 +20,14 @@ const Cinfo = data => {
     document.getElementById('c_city').innerText = info.capital;
     document.getElementById('c_currency').innerText = `${info.currencies[0].name}, ${info.currencies[0].symbol}`;
     document.getElementById('c_code').innerText = `${info.numericCode}`;
-    document.getElementById('c_language').innerText = `${info.languages[0]}`;
+    document.getElementById('c_language').innerText = `${info.languages[0].name}`;
     document.getElementById('c_region').innerText = `${info.region}`;
     document.getElementById('c_subregion').innerText = `${info.subregion}`;
     document.getElementById('c_pop').innerText = `${info.population}`;
     document.getElementById('c_area').innerText = `${info.area}`;
     document.getElementById('c_timezone').innerText = `${info.timezones[0]}`;
     document.getElementById('c_border').innerText = `${info.borders}`;
+    document.getElementById('c_regionalBlock').innerText = `${info.regionalBlocs[0].acronym}, ${info.regionalBlocs[0].name}`;
    document.getElementById('img').setAttribute('src', `${imgg}`);
     document.getElementById('city').value = "";
 }
